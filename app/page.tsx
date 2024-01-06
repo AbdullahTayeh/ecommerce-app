32b003cd1d8b32a263637acab3ce9815ec07d61d
Abdullah Tayeh <abdul.tayeh@outlook.com>
2024-01-06 21:33:11 +0000

    sad

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { client } from "@/sanity/lib/client"
 import { groq } from "next-sanity"
-
 import { SanityProduct } from "@/config/inventory"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,4 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
+  token: "sk5uqiSbt0KkeLCsqWO4p21aTxGxJiJ1T3U6soxPDgcOerD7GwF2N0PBk6xYPPNh9MqriCba85wy0nnFpUPOJQhhOzE5Meo7yIaja5hnTLWp5AVAvWWalpmKOmr0xzP5IRzo5En79bDhqcO8fK6Axp56tXR6j9m2Wgu9XCIDOP9v38qit7sa"
 })
