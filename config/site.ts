5cbbd6ff427abf906d2f8d6331d7dc943802c5d3
Abdullah Tayeh <abdul.tayeh@outlook.com>
2024-01-06 02:42:06 +0000

    Initial commit

---
+++ config/site.ts
@@ -0,0 +1,15 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "VogueVista",
+  description: "Expertly designed goods for the modern wardrobe.",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+
+  ],
+}
