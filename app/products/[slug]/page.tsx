44e8648236a650d76dc17af7a3e18ba9f27fbe3c
AbdullahTayeh <abdul.tayeh@outlook.com>
2024-01-08 04:06:18 +0000

    product gallery

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -28,14 +28,14 @@ const product = await client.fetch<SanityProduct>(
     colors,
     "slug": slug.current
   }`)
-  console.log(product)
+  // console.log(product)
   return (
     <main className="mx-auto max-w-5xl sm:px-6 sm:pt-16 lg:px-8">
       <div className="mx-auto max-w-2xl lg:max-w-none">
         {/* Product */}
         <div className="pb-20 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-12">
           {/* Product gallery */}
-          {/* <ProductGallery /> */}
+          <ProductGallery product={product}/>
           {/* Product info */}
           <ProductInfo product = {product} />
         </div>
