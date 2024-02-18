5ed799da90126ddf1e4884dc12bbd543fe7dd368
Abdullah Tayeh <abdullah.tayeh@jdplc.com>
2024-02-18 12:47:36 +0000

    jkjk

--- main.tf
+++ main.tf
@@ -3,6 +3,24 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" my_bucket {
-    bucket = "VogueVista_s3_bucket"
-  #  acl = "private"
+    bucket = "at-p3_s3_bucket"
+  #  acl = "privat-p3e"
 }
+resource "aws_EC2_instance" "at-p3_EC2_instance_1"{
+  ami = ""
+}
+
+resource "aws_EC2_instance" "at-p3_EC2_instance_2"{
+  ami = ""
+}
+
+resource "aws_lb" "at-p3-lb" {
+  name = "at-p3-loadbalancer"
+  external = false
+  load_balancer_type = "applicat-p3ion"
+  subnet = var.public_sn_id
+  security_groups = toset([var.aws_security_id])
+  enable_deletion_protection = false
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "public_sn_ids" {
+    type = set(string)
+}
