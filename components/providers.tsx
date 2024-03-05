af933f82307bea9a8ef7dfdf395b6632c753b0fc
Abdullah Tayeh <abdullah.tayeh@jdplc.com>
2024-03-05 16:10:33 +0000

    light

--- components/providers.tsx
+++ components/providers.tsx
@@ -16,7 +16,7 @@ export function Providers({ children }: Props) {
     cartMode="checkout-session"
     stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!}
     >
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       <Toaster />
       {children} 
       <TailwindIndicator />
